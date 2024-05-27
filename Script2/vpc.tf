05a633c97bf85a7aafca2a83bdc19958caa081ba
dlimachi <dlimachi@itba.edu.ar>
2024-05-27 20:24:10 -0300

    Added script 2 to deploy an endpoint

---
+++ Script2/vpc.tf
@@ -0,0 +1,32 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+    tags = {
+    Name = "redes-vpc"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+}
