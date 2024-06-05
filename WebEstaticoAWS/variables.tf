231d76591adbfe9deb136fe343d7d346b75641b2
sespina99 <sespina@itba.edu.ar>
2024-06-05 20:30:48 -0300

    forgot vars

---
+++ WebEstaticoAWS/variables.tf
@@ -0,0 +1,9 @@
+variable "domain_name" {
+  type        = string
+  description = "The domain name for the website"
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket to create"
+}
